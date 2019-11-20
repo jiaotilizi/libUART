d65cc1edc4d8eea00c19785626406b8a8706c278
Johannes Krottmayer <krjdev@gmail.com>
2019-11-20 01:47:49 +0100

    add version information header

---
+++ src/libUART/version.h
@@ -0,0 +1,26 @@
+/**
+ *
+ * File Name: version.h
+ * Title    : libUART version information
+ * Project  : libUART
+ * Author   : Copyright (C) 2018-2019 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2019-11-20
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see file LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#ifndef LIBUART_VERSION_H
+#define LIBUART_VERSION_H
+
+#define LIBUART_NAME            "libUART"
+#define LIBUART_VERSION         "0.3.0.0"
+#define LIBUART_COPYRIGHT       "Copyright (C) 2018-2019 Johannes Krottmayer <krjdev@gmail.com>"
+
+#endif
