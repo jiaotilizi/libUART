6b2740265b0498f2c5caf8449e8e5fd3a0fcdfde
Johannes Krottmayer <krjdev@gmail.com>
2019-11-30 03:23:50 +0100

    libUART_demo: add makefile

--- src/libUART_demo/main.c
+++ src/libUART_demo/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 #include <libUART.h>
 
 int main(int argc, char* argv[])
 {
-    printf("%s\r\n", libUART_get_libname());
+    printf("%s %s\r\n", libUART_get_libname(), libUART_get_libversion());
     return 0;
 }
---
+++ src/libUART_demo/run_demo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+LD_LIBRARY_PATH=./../libUART/ ./libUART_demo
