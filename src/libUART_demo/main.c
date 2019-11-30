db3ea6e61142b2ec33385ff71b34b94d63022526
Johannes Krottmayer <krjdev@gmail.com>
2019-11-30 06:54:26 +0100

    libUART_demo: Windows support

--- src/libUART_demo/main.c
+++ src/libUART_demo/main.c
@@ -18,26 +18,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef __unix__
 #include <unistd.h>
 
-#include <libUART.h>
-
 #define UART_DEV    "/dev/ttyUSB0"
+#elif _WIN32
+#include <Windows.h>
+
+#define UART_DEV    "\\\\.\\COM3"
+#endif
+#include <libUART.h>
 
 int main(int argc, char* argv[])
 {
     uart_t *uart;
-    char *buf;
+    char* buf;
     int bytes;
     
     printf("%s %s\r\n", libUART_get_libname(), libUART_get_libversion());
-    uart = libUART_open(UART_DEV, UART_BAUD_115200, "8N1N");
+    uart = libUART_open(UART_DEV, UART_BAUD_9600, "8N1N");
     
     if (!uart)
         return -1;
     
     printf("TX: %d byte(s)\n", libUART_puts(uart, "Hello World!"));
-    sleep(1);
+    Sleep(1000);
     libUART_get_bytes_available(uart, &bytes);
     printf("RX: %d byte(s) available\n", bytes);
     buf = (char *) malloc(bytes + 1);
